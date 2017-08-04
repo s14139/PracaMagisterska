db63b64f3e179a7696968ce5c1b4a458c95e99a6
DESKTOP-2P1JMSD\mckja <s14139@pjwstk.edu.pl>
2017-08-04 20:41:45 +0200

    ng

---
+++ WorkoutTracker2/WorkoutTracker2/Scripts/Controllers/IndexNgController.js
@@ -0,0 +1,8 @@
+﻿var workoutApp = angular.module('workoutApp', []);
+workoutApp.controller('workoutController', ['$scope', function ($scope) {
+
+    $.get("/Workouts/FillIndexKO", {}, function (data) {
+        $scope.workouts = data;
+    });
+    
+}]);
---
+++ WorkoutTracker2/WorkoutTracker2/Scripts/angular/scope.js
@@ -0,0 +1,8 @@
+﻿var workoutApp = angular.module('workoutApp', []);
+workoutApp.controller('workoutController', ['$scope', function ($scope) {
+    $scope.firstName = "Mary";
+    $scope.lastName = "Jane";
+    $.get("/Workouts/FillIndexKO", {}, function (data) {
+        $scope.workouts = data;
+    });
+}]);
