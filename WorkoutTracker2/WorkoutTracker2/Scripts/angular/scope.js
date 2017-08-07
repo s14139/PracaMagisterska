d5e371a23d3f27219470eacd7b40f0993067eaa6
DESKTOP-2P1JMSD\mckja <s14139@pjwstk.edu.pl>
2017-08-08 00:47:55 +0200

    ng almost done

--- WorkoutTracker2/WorkoutTracker2/Scripts/angular/scope.js
+++ WorkoutTracker2/WorkoutTracker2/Scripts/angular/scope.js
@@ -1,8 +1,44 @@
 ﻿var workoutApp = angular.module('workoutApp', []);
+
+workoutApp.directive('mydatepicker', function () {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, element, attrs, ngModelCtrl) {
+            element.datepicker({
+                dateFormat: "dd-mm-yy",
+                onSelect: function (date) {
+                    scope.workoutDate = date;
+                    scope.$apply();
+                }
+            });
+        }
+    };
+});
+
 workoutApp.controller('workoutController', ['$scope', function ($scope) {
-    $scope.firstName = "Mary";
-    $scope.lastName = "Jane";
-    $.get("/Workouts/FillIndexKO", {}, function (data) {
-        $scope.workouts = data;
+    $scope.name = "";
+    $scope.location = "";
+    $scope.workoutDate = "";
+    $scope.workoutLength = "";
+    $scope.locations = [];
+    $scope.addWorkout = function () {
+        var viewModel = {};
+        viewModel.Name = $scope.name;
+        viewModel.Location = $scope.location;
+        viewModel.WorkoutDate = $scope.workoutDate;
+        viewModel.WorkoutLength = $scope.workoutLength;
+        $scope.workouts.push($scope.workouts[0]);
+        //$scope.editWorkout(viewModel)
+    };
+    //$scope.workouts = [{ workoutName: 'asdad', workoutDate: '2016-05-09' }, { workoutName: 'workout2', workoutDate: '2015-01-01' }]
+    jQuery.ajax({
+        url: "/Workouts/FillIndexKO",
+        success: function (result) {
+            $scope.workouts = result;
+            $scope.locations = result[0].Locations;
+        },
+        async: false
     });
 }]);
+
